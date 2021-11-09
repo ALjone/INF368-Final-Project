81190766ed7bc40c109c7155a7ec2464a18cd816
Audun <audun.henriksen@gmail.com>
2021-11-09 12:38:47 +0100

    fix bug

--- GPT2Tuner.py
+++ GPT2Tuner.py
@@ -127,4 +127,4 @@ if __name__ == '__main__':
     tuner = GPT2Tuner(data_path=args.train_data_path, device = args.device, batch_size=int(args.batch_size))
     print("Starting training:")
     tuner.train(int(args.epochs))
-    tuner.save_sentences(args.samples_per_class, path=args.output_dir + "/" + args.output_name)
+    tuner.save_sentences(int(args.samples_per_class), path=args.output_dir + "/" + args.output_name)
