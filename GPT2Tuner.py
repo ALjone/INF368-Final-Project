9c4048c19ffd149b06518cc752bee0bc70fc280e
ALjone <44534676+ALjone@users.noreply.github.com>
2021-11-10 19:00:18 +0100

    fix tuner and delete files

--- GPT2Tuner.py
+++ GPT2Tuner.py
@@ -3,7 +3,6 @@ import warnings
 warnings.filterwarnings('ignore')
 import pandas as pd
 import torch
-from customDataset import customDataset
 import argparse
 import time
 import datetime
@@ -12,7 +11,30 @@ import random
 from transformers import GPT2LMHeadModel, GPT2Config
 from torch.utils.data import DataLoader, RandomSampler
 from transformers import GPT2Tokenizer
+from torch.utils.data import Dataset 
 import gc
+
+class customDataset(Dataset):
+
+    def __init__(self, sentences, tokenizer: GPT2Tokenizer, max_length=1024):
+
+        self.tokenizer = tokenizer 
+        #TODO torch tensor instead?
+        self.input_ids = []
+        self.attn_masks = []
+
+        for sentence in sentences:      
+            encodings = tokenizer(sentence, truncation=True, max_length=min(max_length, 1024), padding="max_length")
+                    
+            self.input_ids.append(torch.tensor(encodings['input_ids']))
+            self.attn_masks.append(torch.tensor(encodings['attention_mask']))
+    
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.attn_masks[idx] 
+
 
 
 class GPT2Tuner:
